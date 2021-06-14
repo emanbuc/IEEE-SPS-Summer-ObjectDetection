25c62831aac4ef214af85bc71264da4eef17be15
emanuele.buchicchio <emanuele.buchicchio@smartpeg.eu>
2021-06-14 09:46:53 +0200

    feat: color mask

--- intermediate-3.py
+++ intermediate-3.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 capDevice = cv.VideoCapture(0, cv.CAP_DSHOW)
 
@@ -7,6 +8,17 @@ while True:
 
     width = int(capDevice.get(3))
     height = int(capDevice.get(3))
+
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    lowerBlue= np.array([90,50,50])
+    upperBlue = np.array([130, 255, 255])
+
+    # create a blue mask to show only blue
+    mask= cv.inRange(hsv, lowerBlue,upperBlue)
+
+    result = cv.bitwise_and(frame,frame, mask=mask)
+    cv.imshow("webcam image",frame)
+    cv.imshow("image with color mask", result)
 
     #some overlay on captured video frame
     imgLine=cv.line(frame, (0, 0), (width,height), (255, 255, 255), 5)
